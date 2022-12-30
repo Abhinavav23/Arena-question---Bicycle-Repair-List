dc82bfa84b349755d8b73eed6474775d2b397426
Abhinav <abhinav@AVs-Mac.local>
2022-12-30 11:00:40 +0530

    [29 dec 2022] Adds logic to add and delete bicycle from list.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import repairList from './reducer';
 
 // Store is defined with the reducer
 
-const store = createStore(repairList,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+const store = createStore(repairList)
 
 ReactDOM.render(
   <Provider store = {store}>
     <App />
-    </Provider>, 
+  </Provider>,
   document.getElementById("root"));
